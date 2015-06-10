4a32490ffad850e65283acde58d9c9e62a9098ed
jithendrae <jithendra.edupuganti@imaginea.com>
2015-06-10 16:57:28 +0530

    Re-Added dependent class

--- restaurantApp/src/main/java/com/imaginea/restaurantApp/RestaurantApp.java
+++ restaurantApp/src/main/java/com/imaginea/restaurantApp/RestaurantApp.java
@@ -311,3 +311,30 @@ public class RestaurantApp {
 				.collect(Collectors.toList());
 	}
 }
+
+final class MenuEntry<K, V> implements Map.Entry<K, V> {
+    private final K key;
+    private V value;
+
+    public MenuEntry(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    @Override
+    public K getKey() {
+        return key;
+    }
+
+    @Override
+    public V getValue() {
+        return value;
+    }
+
+    @Override
+    public V setValue(V value) {
+        V old = this.value;
+        this.value = value;
+        return old;
+    }
+}
